c5bda6234bac23557d381f2532ffa89024b277f6
Ivan <43518470+Thundiverter@users.noreply.github.com>
2023-11-25 03:31:01 +0300

    Added email

--- src/components/FirstSection.vue
+++ src/components/FirstSection.vue
@@ -18,6 +18,7 @@ import ExternalLinkIcon from './icons/ExternalLinkIcon.vue'
 
         <div class="links">
             <a href="https://github.com/Thundiverter" target="_blank">GitHub<ExternalLinkIcon /></a>
+            <a href="mailto:ivan.zubkov.dev@gmail.com">ivan.zubkov.dev@gmail.com<ExternalLinkIcon /></a>
         </div>
     </section>
 </template>
@@ -38,5 +39,11 @@ h1 {
     flex-wrap: wrap;
     gap: .5rem;
     margin: 2rem 0 1.5rem 0;
+}
+
+.links {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 3rem;
 }
 </style>
